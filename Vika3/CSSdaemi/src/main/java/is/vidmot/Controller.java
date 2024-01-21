50fa9923a7626b099e70142e42df193aaac5e67d
Ebba Þóra Hvannberg <ebba@hi.is>
2024-01-21 15:19:46 +0000

    Hreinsað til í stylesheet og útgáfa java er 21

--- Vika3/CSSdaemi/src/main/java/is/vidmot/Controller.java
+++ Vika3/CSSdaemi/src/main/java/is/vidmot/Controller.java
@@ -1,18 +1,19 @@
 package is.vidmot;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller  {
     public TextField fxSimi;
+    public Label nafn;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        fxSimi.getStyleClass().add("text-field-stor");
+
+    public void initialize() {
+
+        fxSimi.getStyleClass().add("text-field-stor"); // style er í EinfaldurCSS.css
     }
 }
