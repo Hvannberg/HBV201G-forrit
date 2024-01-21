62b83212ad7953e25e4fac30f7d4942646740279
Ebba Þóra Hvannberg <ebba@hi.is>
2024-01-21 17:59:56 +0000

    Hreinsað til í stylesheet og útgáfa java er 21

--- Vika3/CSSdaemi/src/main/java/is/vidmot/Controller.java
+++ Vika3/CSSdaemi/src/main/java/is/vidmot/Controller.java
@@ -1,11 +1,7 @@
 package is.vidmot;
 
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class Controller  {
     public TextField fxSimi;
