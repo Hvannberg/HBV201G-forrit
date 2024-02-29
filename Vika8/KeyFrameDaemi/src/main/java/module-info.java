ee7be2ebaefdd3d16ec86a4000278b397b3712a9
Ebba Þóra Hvannberg <ebba@hi.is>
2024-02-29 08:53:13 +0000

    lagfæring module-info.java

--- Vika8/KeyFrameDaemi/src/main/java/module-info.java
+++ Vika8/KeyFrameDaemi/src/main/java/module-info.java
@@ -1,6 +1,9 @@
-module vidmot {
+module is.vidmot {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+
+    exports is.vidmot to javafx.graphics, javafx.fxml;
+    opens is.vidmot to javafx.fxml;
 }
